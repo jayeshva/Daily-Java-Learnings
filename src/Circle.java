a36eb7f7bbcafa3b8b8b7ed940f9585476756e8c
jayeshva <jayesh007va@gmail.com>
2024-07-18 07:41:12 +0530

    java Basics

---
+++ src/Circle.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class Circle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Circle Drawn..");
+    }
+    public void hello()
+    {
+        System.out.println("Hello Circle..");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Shape s1 = new Circle();
+        s1.init();
+        s1.draw();
+    }
+}
